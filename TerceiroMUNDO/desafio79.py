fdb7b8805c069db8351acc42c3752852eb32cda3
JairoJJr <jairojannini.contato@gmail.com>
2023-05-31 22:47:19 -0300

    exercícios

--- TerceiroMUNDO/desafio79.py
+++ TerceiroMUNDO/desafio79.py
@@ -2,3 +2,15 @@
 Caso o número já exita lá dentro, ele não será adicionado.
 No final, serão exibidos todos os valores únicos digitados, em ordem crescente.'''
 
+num = []
+while True:
+    n = int(input("Digite um número: "))
+    if n not in num:
+        num.append(n)
+    resp = str(input("Deseja inserir outro? [s/n]")).lower()
+    while resp not in 'sn':
+        resp = str(input("Dado inválido. Digite [s/n]")).lower()
+    if resp == "n":
+        break
+num.sort(reverse = True)
+print(num)

d0b8631b9b3a00ed686f3dabbb8cd8119c88715c
JairoJJr <jairojannini.contato@gmail.com>
2023-06-10 00:34:50 -0300

    82

--- TerceiroMUNDO/desafio82.py
+++ TerceiroMUNDO/desafio82.py
@@ -1,3 +1,21 @@
 '''Crie um programa que vai ler vários números e colocar em uma lista.
 Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente.
 Ao final, mostre o conteúdo das três listas geradas.'''
+
+num = []
+numpar = []
+numimp = []
+ct = 0
+while True:
+    num.append(int(input("Digite um número: ")))
+    resp = str(input("DEseja inserir outro? [S/N]")).strip().lower()
+    if num[ct] % 2 == 0:
+        numpar.append(num[ct])
+    else:
+        numimp.append(num[ct])
+    while resp not in "sn":
+        resp = str(input("Dado inválido, digite S ou N")).strip().lower()
+    if resp == "n":
+        break
+    ct += 1
+print(num , numpar , numimp)

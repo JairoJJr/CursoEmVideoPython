508b7376a3f3585a94b08a6ffff34b6f46ae664f
JairoJJr <jairojannini.contato@gmail.com>
2023-06-11 15:08:39 -0300

    86

--- TerceiroMUNDO/desafio086.py
+++ TerceiroMUNDO/desafio086.py
@@ -1,2 +1,13 @@
 '''Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado.
 No final, mostre a matriz na tela, com a formatação correta.'''
+
+matriz = [[[],[],[]],[[],[],[]],[[],[],[]]]
+
+for c in range (0,3):
+    for i in range (0,3):
+        matriz[c][i].append(int(input(f'Digite o valor da posição {c + 1,i + 1}: ')))
+for c in range (0,3):
+    for i in range (0,3):
+        print(matriz[c][i], end='')
+    print()
+    

2529752723fefd40d430a74f08b99bbc77ec18d3
JairoJJr <jairojannini.contato@gmail.com>
2023-06-11 14:57:01 -0300

    85

--- TerceiroMUNDO/desafio085.py
+++ TerceiroMUNDO/desafio085.py
@@ -2,3 +2,16 @@
 e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares.
 No final, mostre os valores pares e ímpares em ordem crescente.'''
 
+num = [[],[]]
+
+for c in range (0,7):
+    n  = int(input("Digite um número: "))
+    if n % 2 == 0:
+        num[0].append(n)
+    else:
+        num[1].append(n)
+
+num[0].sort()
+num[1].sort()
+
+print(num)

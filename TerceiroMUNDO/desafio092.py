3fb80b1deadd073795267ef314358f903cb85c27
JairoJJr <jairojannini.contato@gmail.com>
2023-06-15 20:38:07 -0300

    92

--- TerceiroMUNDO/desafio092.py
+++ TerceiroMUNDO/desafio092.py
@@ -4,3 +4,18 @@ o dicionário receberá também o ano de contratação e o salário.
 Calcule e acrescentem além da idade, com quqantos anos a pessoa vai se aposentar.(35 anos de contribuição)'''
 
 import datetime
+
+cadastro = dict()
+
+cadastro['Nome'] = str(input('Nome: '))
+nasc = int(input('Ano de Nascimento: '))
+cadastro['Idade'] = datetime.datetime.now().year - nasc
+cadastro['CTPS'] = int(input('Carteira de Trabalho (0 não tem): '))
+
+if cadastro['CTPS'] != 0:
+    cadastro['Contratação'] = int(input('Ano de Contratação: '))
+    cadastro['Salário'] = f'R${float(input("Salário: R$")):.2f}'
+    cadastro['Aposentadoria'] = (cadastro['Contratação'] + 35) - nasc
+
+for k, v in cadastro.items():
+    print(f'->{k:>15}:{v:>15}')

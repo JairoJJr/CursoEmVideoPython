941575c0636a5c3a8b327f21007e91a30998afeb
JairoJJr <jairojannini.contato@gmail.com>
2023-07-13 17:01:47 -0300

    102

--- TerceiroMUNDO/desafio101.py
+++ TerceiroMUNDO/desafio101.py
@@ -1,13 +1,13 @@
 '''Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa,
 retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGRATÓRIO  nas eleições.'''
-import datetime
 
 def voto(nasc):
-    h = int(datetime.date.today().year)
+    from datetime import date
+    h = int(date.today().year)
     i = h - nasc
     if 18 <= i < 65:
         return(f"Com {i} anos o voto é OBRIGATÓRIO")
-    elif i < 18:
+    elif i < 16:
         return(f"Com {i} anos o voto é NEGADO")
     else:
         return(f"Com {i} anos o voto é OPCIONAL")
--- TerceiroMUNDO/desafio102.py
+++ TerceiroMUNDO/desafio102.py
@@ -1,3 +1,39 @@
 '''Crie um programa que tenha uma função fatorial() que receba dois parâmetros: 
 o primeiro que indique o NÚMERO a calcular e o outro chamado SHOW, que será um valor lógico (opcional)
 indicando se será mostrado ou não na tela o processo de cálculo do fatorial.'''
+
+def fatorial(n=0 , show=False):
+    """
+    Calcula o fatorial de um número.
+    :param n: O número a ser calculado.
+    :param show: (Opcional) Mostrar ou não o cálculo.
+    :return: O valor do fatorial de um número n."""
+    
+    f = 1
+    for i in range (n, 0, -1):
+        f *= i
+    if show == False:
+        return(f'O fatorial de {n} é {f}')
+    else:
+        for i in range(n, 0, -1):
+            if i != 1:
+                print(i, end=' X ')
+            else:
+                print(i,' = ',f)
+
+
+n = int(input('Digite um número: '))
+while True:
+    show = str(input('Ver cálculo? [S/N]: ')).lower().strip()
+    if show not in 'sn':
+        print('ERRO! Digite S ou N')
+    else:
+        if show == 's':
+            show = True
+            fatorial(n, show)
+        else:
+            show = False
+            print(f'{fatorial(n , show)}')
+        break
+
+

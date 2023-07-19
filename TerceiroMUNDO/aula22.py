2135bd41768dfe7bba4655f0a12df0749548b32a
JairoJJr <jairojannini.contato@gmail.com>
2023-07-19 20:37:21 -0300

    aula22

---
+++ TerceiroMUNDO/aula22.py
@@ -0,0 +1,14 @@
+'''
+Modularicação
+
+Começou nos anos 60, por causa dos programas cada vez maiores
+Foco: Dividir um programa grande;
+Aumentar a legibilidade;
+Facilita a manutenção.
+
+Trabalha com pastas. Vide pasta Aula 22
+
+Pacotes
+importa uma 'categoria' de funções para o programa principal.
+Com pastas dentro de pastas vc faz modularização com pacotes (bibliotecas).
+'''
---
+++ TerceiroMUNDO/aula22/uteis/numeros/__init__.py
@@ -0,0 +1,9 @@
+def fatorial(n):
+    f = 1
+    for i in range(n, f, -1):
+        f *= i
+    return f
+
+def dobro(n):
+    d = n *2
+    return d

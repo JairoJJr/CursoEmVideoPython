2a6028eb25c580c0a41d1f3bca40178a174f493b
JairoJJr <jairojannini.contato@gmail.com>
2023-07-19 22:11:52 -0300

    107 ok

--- TerceiroMUNDO/desafio108.py
+++ TerceiroMUNDO/desafio108.py
@@ -1,2 +1,3 @@
-'''Adapte o código do exercício 107, criando uma função adicional chammada moeda ()
+'''Adapte o código do exercício 107, criando uma função adicional chamada moeda()
 que consiga mostra os valores como um valor monetário formatado.'''
+
--- TerceiroMUNDO/ex107/moeda.py
+++ TerceiroMUNDO/ex107/moeda.py
@@ -1,14 +1,18 @@
 def aumentar(num, p):
-    p = p/100
-    aumento = num + (num*p)
+    aumento = num + (num*p/100)
     return aumento
+
+
 def diminuir(num, p):
-    p = p/100
-    dimin = num - (num*p)
+    dimin = num + (num*p/100)
     return dimin
+
+
 def dobro(num):
     d = num*2
     return d
+
+
 def metade(num):
     m = num / 2
     return m

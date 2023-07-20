5c9ab380ad407ff6cef8629bd7af5a1484f2a77c
JairoJJr <jairojannini.contato@gmail.com>
2023-07-20 08:26:41 -0300

    desafios 133 114 115

---
+++ TerceiroMUNDO/desafio115.py
@@ -0,0 +1,4 @@
+'''Crie um pequeno sistema modularizado que permita cadastrar pessoas pelo seu Nome e Idade em um arquivo de texto simples.
+O sistema só vai ter 2 opções: 
+Cadastrar uma nova pessoa;
+Listar todas as pessoas.'''

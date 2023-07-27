f71ab588d5e7ae08183ce52c09002406d4a87dfb
JairoJJr <jairojannini.contato@gmail.com>
2023-07-26 21:00:18 -0300

    114

--- TerceiroMUNDO/desafio114.py
+++ TerceiroMUNDO/desafio114.py
@@ -1,1 +1,12 @@
 '''Crie um código em Python que teste se o site Pudim está acessível pelo computador usado.'''
+
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except:
+    print('\033[31mDeu erro\033[m')
+else:
+    print("\033[32mO site está funcionando\033[m")
+    print(site.read())
